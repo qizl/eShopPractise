ec9ef0da2777f5fd4cde2f22750356574cfe35d8
qizl <q@qizl.cn>
2018-07-09 19:26:01 +0800

    -

--- Web/src/Web/Interfaces/IBasketService.cs
+++ Web/src/Web/Interfaces/IBasketService.cs
@@ -3,7 +3,7 @@ using EnjoyCodes.eShopOnWeb.Web.ViewModels;
 
 namespace EnjoyCodes.eShopOnWeb.Web.Interfaces
 {
-    public interface IBasketService
+    public interface IBasketViewModelService
     {
         Task<BasketViewModel> GetOrCreateBasketForUser(string userName);
     }
--- Web/src/Web/ViewComponents/Basket.cs
+++ Web/src/Web/ViewComponents/Basket.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EnjoyCodes.eShopOnWeb.Infrastructure.Identity;
+using EnjoyCodes.eShopOnWeb.Web.Interfaces;
+using Microsoft.AspNetCore.Identity;
 
 namespace EnjoyCodes.eShopOnWeb.Web.ViewComponents
 {
     public class Basket
     {
+        private readonly IBasketViewModelService _basketService;
+        private readonly SignInManager<ApplicationUser> _signInManager;
     }
 }
